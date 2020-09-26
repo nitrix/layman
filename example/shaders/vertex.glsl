58bdc0bdf229e297bd08cd759e7960ddc2100c9d
nitrix <nitrix@nitrix.me>
2020-09-26 18:24:59 -0400

    Small cleanup vert glsl.
    Former-commit-id: bed1973591b250f83198d20956df5d5903efab7e [formerly 3ca95fb83fa5013cdf2f9adc9492009a5ff8b557]
    Former-commit-id: 859f4d82c8786d4c07147e46e9780b965867fb0f

--- example/shaders/vertex.glsl
+++ example/shaders/vertex.glsl
@@ -1,25 +1,25 @@
 #version 330 core
 
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 uv;
-layout (location = 2) in vec3 normal;
-layout (location = 3) in vec4 tangent;
+in vec3 position;
+in vec2 uv;
+in vec3 normal;
+in vec4 tangent;
+
+out vec2 UV;
+out vec3 WorldPosition;
+out mat3 TBN;
 
 uniform mat4 model_transform;
 uniform mat4 normal_transform;
 uniform mat4 view_transform;
 uniform mat4 projection_transform;
 
-out vec2 UV;
-out vec3 WorldPosition;
-out mat3 TBN;
-
 void main() {
     gl_Position = projection_transform * view_transform * model_transform * vec4(position, 1.0);
     WorldPosition = vec3(model_transform * vec4(position, 1.0));
     UV = uv;
 
-    // TBN: Tangent space to world space.
+    // Generate TBN (tangent space to world space conversion matrix).
     vec3 T = normalize(mat3(normal_transform) * tangent.xyz);
     vec3 N = normalize(mat3(normal_transform) * normal);
     vec3 B = normalize(cross(T, N));
