0d5c8ea8bf893f4ea2828e061663ef1560616480
Alex Belanger <nitrix@nitrix.me>
2020-11-05 15:27:05 -0500

    Added some timings to understand long start times.
    Former-commit-id: ae84ed1abdd86d6a4a145221dd389fe46c8dd232 [formerly 218a88b28c308e8955d15008a34dcb733e49c629]
    Former-commit-id: 8ee074f87a1febbdc2d4fdd0f608be5ff3715813

--- example/main.go
+++ example/main.go
@@ -11,20 +11,23 @@ import (
 
 func main() {
 	// Create a window.
-	//window, err := laygl.NewFullScreenWindow("Demo")
-	window, err := laygl.NewWindow(1280, 720, "Demo")
+	t := time.Now()
+	window, err := laygl.NewWindow(1280, 720, "Demo", false)
 	if err != nil {
 		log.Fatalln("Unable to create fullscreen window:", err)
 	}
-	defer window.Destroy()
+	log.Println("Time to create the window:", time.Now().Sub(t))
 
 	// Create the renderer.
+	t = time.Now()
 	renderer, err := laygl.NewRenderer(window)
 	if err != nil {
 		log.Fatalln("Unable to create renderer:", err)
 	}
+	log.Println("Time to create the renderer:", time.Now().Sub(t))
 
 	// Load the model.
+	t = time.Now()
 	model, err := laygl.LoadModel("helmet", "assets/DamagedHelmet.glb")
 	//model, err := laygl.LoadModel("helmet", "assets/cowboy.glb")
 	//model, err := laygl.LoadModel("helmet", "assets/helmet.glb")
@@ -36,6 +39,7 @@ func main() {
 	if err != nil {
 		log.Fatalln("Unable to load model:", err)
 	}
+	log.Println("Time to load the model:", time.Now().Sub(t))
 
 	// Create directional light
 	directionalLight := lights.NewDirectional()
@@ -70,14 +74,16 @@ func main() {
 	//scene.AddLight(&pointLight)
 
 	// Reduce the memory residency after loading assets into GPU memory.
+	t = time.Now()
 	debug.FreeOSMemory()
+	log.Println("Time to free up memory:", time.Now().Sub(t))
 
 	fps := 0
 	lastTime := time.Now()
 
 	entity.Translate(0, 0, -3)
 
-	window.OnRender(func() {
+	window.Run(func() {
 		// Update.
 		entity.Rotate(0, window.Elapsed() * 0.5, 0)
 
@@ -92,6 +98,10 @@ func main() {
 			lastTime = time.Now()
 		}
 	})
+}
 
-	window.Run()
+func wrapTiming(do func()) time.Duration {
+	t := time.Now()
+	do()
+	return time.Now().Sub(t)
 }
