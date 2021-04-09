c63fe6a9806f1aec30734a77be0b5abba56645a3
Alex Belanger <nitrix@nitrix.me>
2021-04-08 20:29:46 -0400

    Working on rotation gizmo; updated screenshot.

--- include/utils.h
+++ include/utils.h
@@ -18,7 +18,10 @@
 
 void utils_render_cube(void);
 void utils_render_line(vec3 from, vec3 to, mat4 transform, vec4 color);
-void utils_render_ngon(vec3 origin, int n, float radius, mat4 transform, vec4 color);
+void utils_render_ngon(int n, float radius, mat4 transform, vec4 color);
+float utils_closest_distance_between_two_rays(vec3 r1_origin, vec3 r1_direction, vec3 r2_origin, vec3 r2_direction, float *d1, float *d2);
+float utils_closest_distance_between_ray_and_circle(vec3 ray_origin, vec3 ray_direction, vec3 circle_origin, vec3 circle_orientation, float circle_radius, vec3 closest);
+bool utils_ray_plane_intersection(vec3 plane_origin, vec3 plane_normal, vec3 ray_origin, vec3 ray_direction, float *t);
 struct entity *find_selected_entity(void);
 
 #endif
