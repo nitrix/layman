a6970b5964f26192654ec9c4f57b89db200ca958
Alex Belanger <nitrix@nitrix.me>
2021-04-10 11:51:48 -0400

    Toolkit becoming its own library.

--- lib/toolkit/include/tk/file.h
+++
@@ -1,6 +0,0 @@
-#ifndef TOOLKIT_FILE_H
-#define TOOLKIT_FILE_H
-
-char *tk_file_get_content(const char *filepath);
-
-#endif
--- lib/toolkit/include/tk/time.h
+++
@@ -1,6 +0,0 @@
-#ifndef TOOLKIT_TIME_H
-#define TOOLKIT_TIME_H
-
-void time_millisleep(int msecs);
-
-#endif
